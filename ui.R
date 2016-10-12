ef33f64e57a3503cdcda6e83f52526222c9745ef
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-12 02:23:07 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(gplots)
-diff_exp_pepino <- read.csv("pepino_deg.csv") #this should have github repository link
+diff_exp_pepino <- read.csv("pepino_dge.csv") #this should have github repository link
 contigs <- diff_exp_pepino[,1]
 diff_exp_pepino_df <- cbind(diff_exp_pepino[,2], diff_exp_pepino[,3], diff_exp_pepino[,4])
 rownames(diff_exp_pepino_df)<-contigs
