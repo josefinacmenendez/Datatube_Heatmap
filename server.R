03eb03901948a834e78b9962552273ac9b8f7372
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-13 12:19:49 -0500

    Update server.R

--- server.R
+++ server.R
@@ -2,6 +2,12 @@ library(shiny)
 library(gplots)
 # use the below options code if you wish to increase the file input limit, in this example file input limit is increased from 5MB to 9MB
 options(shiny.maxRequestSize=30*1024^2)
+diff_exp_pepino <- read.csv("pepino_dge.csv") #this should have github repository link
+contigs <- diff_exp_pepino[,1]
+diff_exp_pepino_df <- cbind(diff_exp_pepino[,2], diff_exp_pepino[,3], diff_exp_pepino[,4])
+rownames(diff_exp_pepino_df)<-contigs
+colnames(diff_exp_pepino_df)<-c("Day2","Day12","Day20")
+contigs <- as.vector(contigs)
 
 
 shinyServer(function(input,output){
