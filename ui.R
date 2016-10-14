ae9acc2818db59adffdd1a7a02f126afcb60ff25
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-14 15:35:30 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ shinyUI(fluidPage(
   titlePanel("Select Contigs"),
   sidebarLayout(
     sidebarPanel(
-      selectizeInput("contigs", "contigs 1 - 1000", 
+      selectizeInput("contigs", "contigs 1 - 1001", 
                      multiple = TRUE, 
                      choices = contigs[1:1000], #need to be able to parse the entire list without crashing
                      options=list(maxItems=20))
