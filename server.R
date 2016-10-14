5d6c3653141b5474165d06951dccb19567a641e6
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-14 00:27:00 -0500

    Added input validation

--- server.R
+++ server.R
@@ -12,14 +12,9 @@ contigs <- as.vector(contigs)
 
 shinyServer(function(input,output){
   #output$contig <- renderText(class(input$contigs))This is good for debugging
-  output$contig <- renderText({
-    if(input$contigs =='')
-    {paste("Select at least two contigs")}
-    else
-    {print("Histogram for differential gene expression for the following contigs: ")
-      input$contigs}
+  output$histogram <- renderPlot({
+    validate(need(length(input$contigs)>1,message = "Select at least 2 contigs and 20 contigs at most")) #input validation
+    heatmap.2(diff_exp_pepino_df[c(input$contigs),], trace = "none", col = redblue(20)) #generate heatmap
   })
-
-  output$histogram <- renderPlot({heatmap.2(diff_exp_pepino_df[c(input$contigs),], trace = "none", col = redblue(20))})
 
 })
