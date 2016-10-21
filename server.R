ec1f78f7a848285927475ea37cb296107a4931cc
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-20 21:25:11 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,8 +1,14 @@
 #server.R
+
+#increase file input size
+#options(shiny.maxRequestSize=30*1024^2)
+
 shinyServer(function(input,output){
-  #output$contig <- renderText(class(input$contigs))This is good for debugging
   output$histogram <- renderPlot({
-    validate(need(length(input$contigs)>1,message = "Select at least 2 contigs and 20 contigs at most")) #input validation
-    heatmap.2(diff_exp_pepino_df[c(input$contigs),], trace = "none", col = redblue(20)) #generate heatmap
-  })
+    validate(need(length(input$contigs)>1,message = "Select at least 2 contigs and 50 contigs at most"))
+    heatmap.2(diff_exp_pepino_df[c(input$contigs),], trace = "none", col = redblue(20),
+              cexCol = 2,
+              cexRow = 0.2 + 1/log10(length(input$contigs)),
+              margins = c(8,20))
+  }, height = 700, width = 1000)
 })
