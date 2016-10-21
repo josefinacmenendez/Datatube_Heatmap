bd5f7087287ea6fc710d865b7701dd600cc8fe37
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-20 21:25:40 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,15 +1,16 @@
 #ui.R
+
 shinyUI(fluidPage(
-  titlePanel("Select Contigs"),
+  headerPanel("Select Contigs"),
   sidebarLayout(
     sidebarPanel(
-      selectizeInput("contigs", "Choose up to 50 contigs", 
-                     multiple = TRUE, 
-                     choices = contigs, #All items are available; the list displays items 1 - 1001, but the user can input a name that is not shown
-                     options=list(maxItems=50))
-                 ),              
+      selectizeInput("contigs", 'choose up to 50 contigs',
+                     multiple = TRUE,
+                     choices = contigs[1:100],
+                     options=list(maxItems=50)), width = 2  
+            ),
+    
     mainPanel(
-      textOutput("contig"),
       plotOutput("histogram")
       )
     )
