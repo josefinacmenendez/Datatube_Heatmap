00421f4f9277a1d71a22642b57e972adfdc387d2
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-20 21:26:50 -0500

    Update global.R

--- global.R
+++ global.R
@@ -8,4 +8,5 @@ contigs <- diff_exp_pepino[,1]
 diff_exp_pepino_df <- cbind(diff_exp_pepino[,2], diff_exp_pepino[,3], diff_exp_pepino[,4])
 rownames(diff_exp_pepino_df)<-contigs
 colnames(diff_exp_pepino_df)<-c("Day2","Day12","Day20")
+
 contigs <- as.vector(contigs)
