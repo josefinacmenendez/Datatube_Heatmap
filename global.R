5375fdabd08158c06d0fdc408c81ee356acfa34f
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-25 17:09:06 -0500

    Update global.R

--- global.R
+++ global.R
@@ -2,11 +2,28 @@
 
 library(shiny)
 library(gplots)
+library(functional)
 
-diff_exp_pepino <- read.csv("pepino_dge.csv") #this should have github repository link
-contigs <- diff_exp_pepino[,1]
-diff_exp_pepino_df <- cbind(diff_exp_pepino[,2], diff_exp_pepino[,3], diff_exp_pepino[,4])
-rownames(diff_exp_pepino_df)<-contigs
-colnames(diff_exp_pepino_df)<-c("Day2","Day12","Day20")
+#read data files
+nvs2 <- read.csv("N_vs_day2_whole_list.csv")
+nvs12<- read.csv("N_vs_day12_whole_list.csv")
+nvs20<- read.csv("N_vs_day20_whole_list.csv")
 
+#copy contig names
+contig.number <- nvs2$id
+
+#copy log2Foldchange
+nvs2.log2Foldchange <- nvs2$log2FoldChange
+nvs12.log2Foldchange<- nvs12$log2FoldChange
+nvs20.log2Foldchange<- nvs20$log2FoldChange
+
+dge_pepino <- cbind(nvs2.log2Foldchange,nvs12.log2Foldchange,nvs20.log2Foldchange)
+colnames(dge_pepino) <- c("Day2","Day12","Day20")
+rownames(dge_pepino) <- contig.number
+
+#Remove non-numeric values
+dge_pepino <- dge_pepino[apply(dge_pepino, 1, Compose(is.finite, all)), , drop=FALSE]
+
+#Create vector with contig names
+contigs <- row.names(dge_pepino)
 contigs <- as.vector(contigs)
