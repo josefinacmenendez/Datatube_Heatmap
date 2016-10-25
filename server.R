2c1ef3c33f5317476e86eca5fdfa50459179cfb0
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-25 17:10:03 -0500

    Update server.R

--- server.R
+++ server.R
@@ -2,13 +2,14 @@
 
 #increase file input size
 #options(shiny.maxRequestSize=30*1024^2)
-
-shinyServer(function(input,output){
+shinyServer(function(input,output,session){
+  
+  #Update input according to user's selection
+  updateSelectizeInput(session, 'contigs', choices = contigs, server = TRUE, options = list(maxItems=50))
+  
+  #output$contig <- renderText(class(input$contigs))This is good for testing outputs
   output$histogram <- renderPlot({
-    validate(need(length(input$contigs)>1,message = "Select at least 2 contigs and 50 contigs at most"))
-    heatmap.2(diff_exp_pepino_df[c(input$contigs),], trace = "none", col = redblue(20),
-              cexCol = 2,
-              cexRow = 0.2 + 1/log10(length(input$contigs)),
-              margins = c(8,20))
-  }, height = 700, width = 1000)
+    validate(need(length(input$contigs)>1,message = "Select at least 2 contigs and 20 contigs at most")) #input validation
+    heatmap.2(dge_pepino[c(input$contigs),], trace = "none", col = redblue(20)) #generate heatmap
+  })
 })
