8c3d8b15a28c7631beaebf493c86b11bc8f7c3f2
Josefina Correa Menéndez <josefina.correa@upr.edu>
2016-10-25 23:40:54 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -10,7 +10,9 @@ shinyUI(
       selectizeInput("contigs", "Select up to 50 contigs", 
                      choices = NULL, #need to be able to parse the entire list without crashing
                      options=list(maxOptions = 100),
-                     multiple = TRUE)
+                     multiple = TRUE),
+      #radioButtons(inputId = "imageType", label = "Select the file type", choices = list("jpeg", "pdf")),
+      downloadButton(outputId = "dld", label = "Export as pdf")
                  ),              
     mainPanel(
       plotOutput("histogram")
